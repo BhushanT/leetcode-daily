3427f193f158924c27db3b0afc258844629bda38
Bhushan Thumsi <f20171167@pilani.bits-pilani.ac.in>
2025-01-02 01:24:17 +0000

    Time: 22 ms (49.03%) | Memory: 50.3 MB (19.78%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -1,24 +1,28 @@
 class Solution:
-    def vowelStrings(
-        self, words: List[str], queries: List[List[int]]
-    ) -> List[int]:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        vowels = set('aeiou')
         ans = [0] * len(queries)
-        vowels = {"a", "e", "i", "o", "u"}
-        prefix_sum = [0] * len(words)
-        sum = 0
-        for i in range(len(words)):
-            current_word = words[i]
-            if (
-                current_word[0] in vowels
-                and current_word[len(current_word) - 1] in vowels
-            ):
-                sum += 1
-            prefix_sum[i] = sum
 
-        for i in range(len(queries)):
-            current_query = queries[i]
-            ans[i] = prefix_sum[current_query[1]] - (
-                0 if current_query[0] == 0 else prefix_sum[current_query[0] - 1]
-            )
+        # [2,5] = [0,5] - [0,1]
+
+        dp = [0] * len(words)
+
+        for i,word in enumerate(words):
+            if i == 0:
+                if word[0] in vowels and word[-1] in vowels:
+                    dp[i] = 1
+                else:
+                    dp[i] = 0
+            else:
+                temp = 0
+                if word[0] in vowels and word[-1] in vowels:
+                    temp = 1
+                dp[i] = dp[i - 1] + temp 
+                
+        for i,query in enumerate(queries):
+            if query[0] == 0:
+                ans[i] = dp[query[1]]
+            else:
+                ans[i] = dp[query[1]] - dp[query[0] - 1]
 
         return ans
